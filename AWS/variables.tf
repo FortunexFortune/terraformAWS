cd7e192352eb4e8e1cdd11dd6c7ab91ed0911da5
FortunexFortune <33355125+FortunexFortune@users.noreply.github.com>
2019-04-15 15:01:10 +0100

    Update variables.tf

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -1,11 +1,9 @@
 #Root variables
 
 variable  "aws_access_key" {
-    # default = "AKIAUBR3MSP4AVWUTOGY"
     }
 
 variable  "aws_secret_key" {
-    # default = "EjqC3HKL7LakJ1v8z9hdjjCHCNFSPC+79NeK0DNA"
 }
 
 variable "subnet_cidrs" {
